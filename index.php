236d12616bbb0e04a681a42b7519c6ba24d9287d
rahulemathi <erahul096@gmail.com>
2024-02-08 13:48:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <!-- style and other link -->
+    <?php include "./includes/link.php";?> 
+</head>
+<body>
+<!-- navbar -->
+<?php include "./includes/navbar.php";?> 
+    
+    
+
+
+
+
+
+
+
+<!-- footer -->
+<?php include "./includes/footer.php";?> 
+<!-- scripts -->
+<?php include "./includes/scripts.php";?> 
+</body>
+</html>
